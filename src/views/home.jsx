dce7e33deaa7bfe731fdb1faa7dea721f95e0965
ZuhairZaki <zuhairzaki@gmail.com>
2022-07-15 06:18:46 +0600

    cdi

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
 import NavBar from "../components/navbar";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import TopicIcon from "../components/home/topic";
 
 class Home extends Component {
-  state = {};
+  state = { list : [
+    { name: "Vocabulary-I", icon: "icon-vocabulary" },
+    { name: "Sentence-I", icon: "icon-sentence" },
+    { name: "Vocabulary-II", icon: "icon-vocabulary" },
+    { name: "Sentence-II", icon: "icon-sentence" }
+  ]};
+
   render() {
+    const listItems = this.state.list.map((item, index) => (
+      <Row className="m-4">
+        <Col className="d-flex justify-content-center"><TopicIcon icon={item.icon} topicName={item.name}/></Col>
+      </Row>
+    ))
+
     return (
       <React.Fragment>
         <NavBar />
-        <h1>Home</h1> 
+        <Container>
+          <Row>
+            <Col className="shadow p-4 m-5 bg-white rounded">
+              <Container>
+                <Row className="m-4">
+                  <Col className="d-flex justify-content-center"><h3>Topics</h3></Col>
+                </Row>
+                { listItems }
+              </Container>
+            </Col>
+            <Col className="shadow p-4 m-5 bg-white rounded d-flex justify-content-center">
+              <h3>Stats</h3>
+            </Col>
+          </Row>
+        </Container>
       </React.Fragment>
     );
   }
