b79686d3763ee29e299b2de108d78ee9d5a95892
ZuhairZaki <zuhairzaki@gmail.com>
2022-08-25 22:05:55 +0600

    group word

--- src/views/groupwords.jsx
+++ src/views/groupwords.jsx
@@ -118,7 +118,7 @@ class GroupWords extends Component {
     const catDivs = this.state.cat.map((c) => {
       return (
         <div
-          className="droppable category-container"
+          className="droppable"
           onDragOver={(e) => this.onDragOver(e)}
           onDrop={(e) => this.onDrop(e, c)}
         >
